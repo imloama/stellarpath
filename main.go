dce80b636b357724727e28544ac20939b3eaf130
max <imloama@gmail.com>
2019-03-10 21:44:31 +0800

    feat: use dep

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+import (
+    "log"
+    "github.com/stellar/go/keypair"
+)
+
+func main(){
+	pair, err := keypair.Random()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Println(pair.Seed())
+    log.Println(pair.Address())
+}
