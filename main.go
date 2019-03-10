670d41ad134e2284b1335b3db09a27f33aa5918e
max <imloama@gmail.com>
2019-03-10 21:56:58 +0800

    feat: add toml config

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 import (
+	"fmt"
     "log"
     "github.com/stellar/go/keypair"
+	"github.com/jinzhu/configor"
 )
 
+var Config = struct {
+	
+	Secret String
+	
+	Assets []struct{
+		Code string
+		Issuer string
+	}
+	
+	Paths []string
+	
+	/*
+	APPName string `default:"app name"`
+
+	DB struct {
+		Name     string
+		User     string `default:"root"`
+		Password string `required:"true" env:"DBPassword"`
+		Port     uint   `default:"3306"`
+	}
+
+	Contacts []struct {
+		Name  string
+		Email string `required:"true"`
+	}
+	*/
+}{}
+
 func main(){
+	configor.Load(&Config, "config.toml")
+	fmt.Printf("config: %#v", Config)
+
 	pair, err := keypair.Random()
     if err != nil {
         log.Fatal(err)
